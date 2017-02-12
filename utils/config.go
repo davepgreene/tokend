f3c502639586db9148b2efb24c2f08c1565e49c7
Dave Greene <davepgreene@gmail.com>
2017-02-11 19:31:56 -0500

    Added config defaults and a sample dev config file.

---
+++ utils/config.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+// Defaults generates a set of default configuration options
+func Defaults() {
+	localhost := "127.0.0.1"
+
+	viper.SetDefault("vault", map[string]interface{}{
+		"host":                  localhost,
+		"port":                  8200,
+		"token_renew_increment": 60,
+		"tls": false,
+	})
+
+	viper.SetDefault("metadata", map[string]interface{}{
+		"host": "169.254.169.254",
+		"port": 80,
+	})
+
+	viper.SetDefault("service", map[string]interface{}{
+		"host": localhost,
+		"port": 4500,
+	})
+
+	viper.SetDefault("log", map[string]interface{}{
+		"level":    log.InfoLevel,
+		"json":     true,
+		"requests": true,
+	})
+
+	viper.SetDefault("warden", map[string]interface{}{
+		"host": localhost,
+		"port": 8705,
+		"path": "/v1/authenticate",
+	})
+}
